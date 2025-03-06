28307e609b4f48fac2a17f58c0683d50230fcc7b
chautdn <chautdnde180063@fpt.edu.vn>
2025-03-06 17:14:52 +0700

    fix env

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-require("dotenv").config();
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: "0.0.0.0", // Allows external access (needed for Render)
-    // eslint-disable-next-line no-undef
-    port: process.env.PORT || 3000, // Uses Render's assigned port
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000,
+  },
+  define: {
+    "process.env": {}, // Ensures no unexpected dotenv references
   },
 });
