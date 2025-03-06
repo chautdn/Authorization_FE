b8ca4dc20a13c69b346ae1505c4399374ba2c7e9
chautdn <chautdnde180063@fpt.edu.vn>
2025-03-06 17:11:42 +0700

    fix port

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+require("dotenv").config();
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // Allows external access (needed for Render)
+    // eslint-disable-next-line no-undef
+    port: process.env.PORT || 3000, // Uses Render's assigned port
+  },
 });
